8918e375eff90045e9c45de20926befd96145392
Sasha Koss <koss@nocorp.me>
2025-08-19 03:45:15 +0000

    Update the dev container setup

--- .devcontainer/scripts/on-update.sh
+++ .devcontainer/scripts/on-update.sh
@@ -4,7 +4,24 @@
 
 set -e
 
-eval "$(mise activate bash --shims)"
+# Pull git submodules
+git submodule update --recursive --init --remote
+
+# Trust all mise configs
+mise trust --yes --all
+git submodule foreach --recursive "mise trust"
 
 # Update mise
 mise self-update -y
+
+# Install stack
+mise install
+
+# Install dependencies
+if [ -f ./pnpm-lock.yaml ]; then
+  yes | pnpm install
+else if [ -f ./yarn.lock ]; then
+  yes | yarn install
+else if [ -f ./package-lock.json ]; then
+  yes | npm install
+fi
